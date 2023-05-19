0b20afc66872ac9ad0c6367fffe68f444a2a6dc5
Veyluna <100540704+Veyluna@users.noreply.github.com>
2023-05-19 15:20:43 +1200

    Stoopid goddam pound symbol, chatgpt stoopid

--- src/pages/CategoryProducts.js
+++ src/pages/CategoryProducts.js
@@ -10,121 +10,141 @@ import { ProductStore } from "../data/ProductStore";
 import styles from "./CategoryProducts.module.css";
 
 const CategoryProducts = () => {
+  const params = useParams();
+  const cartRef = useRef();
+  const products = ProductStore.useState((s) => s.products);
+  const shopCart = CartStore.useState((s) => s.product_ids);
+  const [category, setCategory] = useState({});
+  const [searchResults, setSearchResults] = useState([]);
+  const [amountLoaded, setAmountLoaded] = useState(6);
+  const [priceFilter, setPriceFilter] = useState(""); // New state for price filter
 
-    const params = useParams();
-    const cartRef = useRef();
-    const products = ProductStore.useState(s => s.products);
-    const shopCart = CartStore.useState(s => s.product_ids);
-    const [ category, setCategory ] = useState({});
-    const [ searchResults, setsearchResults ] = useState([]);
-    const [ amountLoaded, setAmountLoaded ] = useState(6);
-    const [priceFilter, setPriceFilter] = useState(""); // Add priceFilter state
+  useEffect(() => {
+    const categorySlug = params.slug;
+    const tempCategory = products.find((p) => p.slug === categorySlug);
+    setCategory(tempCategory);
+    setSearchResults(tempCategory.products);
+  }, [params.slug, products]);
 
-    useEffect(() => {
+  const fetchMore = (e) => {
+    setAmountLoaded((prevAmount) => prevAmount + 6);
+    e.target.complete();
+  };
 
-        const categorySlug = params.slug;
-        const tempCategory = products.filter(p => p.slug === categorySlug)[0];
-        setCategory(tempCategory);
-        setsearchResults(tempCategory.products);
-    }, [ params.slug ]);
+  const search = (e) => {
+    const searchVal = e.target.value.toLowerCase();
 
-    const fetchMore = async (e) => {
-
-		//	Increment the amount loaded by 6 for the next iteration
-		setAmountLoaded(prevAmount => (prevAmount + 6));
-		e.target.complete();
-	}
-
-    const search = async e => {
-
-        const searchVal = e.target.value;
-
-        if (searchVal !== "") {
-         
-            const tempResults = category.products.filter(p => p.name.toLowerCase().includes(searchVal.toLowerCase()));
-            setsearchResults(tempResults);
-        } else {
-
-            setsearchResults(category.products);
-        }
-    }
-
-    const handlePriceFilterChange = (e) => {
-        const selectedFilter = e.target.value;
-    
-        // Apply price filter to searchResults
-        let filteredResults = [...category.products];
-    
-        if (selectedFilter === "low") {
-          filteredResults.sort((a, b) => a.price - b.price);
-        } else if (selectedFilter === "high") {
-          filteredResults.sort((a, b) => b.price - a.price);
-        }
-    
-        setsearchResults(filteredResults);
-        setPriceFilter(selectedFilter);
-    };
-    
-      return (
-        <IonPage id="category-page" className={styles.categoryPage}>
-          <IonHeader>
-            <IonToolbar>
-            <IonButtons slot="start">
-                        <IonButton color="dark" text={ category.name } routerLink="/" routerDirection="back">
-                            <IonIcon color="dark" icon={ chevronBackOutline } />&nbsp;Categories
-                        </IonButton>
-                    </IonButtons>
-					<IonTitle>{ category && category.name }</IonTitle>
-              <IonButtons slot="end">
-              <IonBadge color="dark">
-                            { shopCart.length }
-                        </IonBadge>
-						<IonButton color="dark" routerLink="/cart">
-							<IonIcon ref={ cartRef } className="animate__animated" icon={ cart } />
-						</IonButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
-    
-          <IonContent fullscreen>
-            <IonSearchbar className={styles.search} onKeyUp={search} placeholder="Try 'high back'" searchIcon={searchOutline} animated={true} />
-    
-            {/* Add price filter dropdown */}
-            <IonItem>
-              <IonLabel>Price Filter</IonLabel>
-              <IonSelect value={priceFilter} placeholder="Select" onIonChange={handlePriceFilterChange}>
-                <IonSelectOption value="low">Low to High</IonSelectOption>
-                <IonSelectOption value="high">High to Low</IonSelectOption>
-              </IonSelect>
-            </IonItem>
-    
-            <IonGrid>
-
-                <IonRow className="ion-text-center">
-                    <IonCol size="12">
-                         <IonNote>{ searchResults && searchResults.length } { (searchResults.length > 1 || searchResults.length === 0) ? " products" : " product" } found</IonNote>
-                    </IonCol>
-                </IonRow>
-
-                <IonRow>
-                    { searchResults && searchResults.map((product, index) => {
-
-                        if ((index <= amountLoaded) && product.image) {
-                            return (
-                                <ProductCard key={ `category_product_${ index }`} product={ product } index={ index } cartRef={ cartRef } category={ category } />
-                            );
-                        }
-                    })}
-                </IonRow>
-            </IonGrid>
-
-<IonInfiniteScroll threshold="100px" onIonInfinite={ fetchMore }>
-<IonInfiniteScrollContent loadingSpinner="bubbles" loadingText="Fetching more...">
-</IonInfiniteScrollContent>
-</IonInfiniteScroll>
-          </IonContent>
-        </IonPage>
+    if (searchVal !== "") {
+      const tempResults = category.products.filter((p) =>
+        p.name.toLowerCase().includes(searchVal)
       );
-    };
-    
-    export default CategoryProducts;
+      setSearchResults(tempResults);
+    } else {
+      setSearchResults(category.products);
+    }
+  };
+
+  const applyPriceFilter = () => {
+    let filteredResults = [...searchResults]; // Create a copy of searchResults
+  
+    if (priceFilter === "low") {
+      filteredResults.sort((a, b) => parseFloat(a.price.substring(1)) - parseFloat(b.price.substring(1)));
+    } else if (priceFilter === "high") {
+      filteredResults.sort((a, b) => parseFloat(b.price.substring(1)) - parseFloat(a.price.substring(1)));
+    }
+    console.log(filteredResults);
+    setSearchResults(filteredResults);
+  };
+
+  return (
+    <IonPage id="category-page" className={styles.categoryPage}>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton
+              color="dark"
+              text={category.name}
+              routerLink="/"
+              routerDirection="back"
+            >
+              <IonIcon color="dark" icon={chevronBackOutline} />
+              &nbsp;Categories
+            </IonButton>
+          </IonButtons>
+          <IonTitle>{category && category.name}</IonTitle>
+
+          <IonButtons slot="end">
+            <IonBadge color="dark">{shopCart.length}</IonBadge>
+            <IonButton color="dark" routerLink="/cart">
+              <IonIcon
+                ref={cartRef}
+                className="animate__animated"
+                icon={cart}
+              />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonSearchbar
+          className={styles.search}
+          onKeyUp={search}
+          placeholder="Try 'high back'"
+          searchIcon={searchOutline}
+          animated={true}
+        />
+
+        {/* Price Filter Dropdown */}
+        <IonGrid className={styles.priceFilterGrid}>
+          <IonRow>
+            <IonCol>
+              <IonSelect value={priceFilter} placeholder="Sort by Price" onIonChange={(e) => setPriceFilter(e.target.value)}>
+                <IonSelectOption value="low">Lowest Price</IonSelectOption>
+                <IonSelectOption value="high">Highest Price</IonSelectOption>
+              </IonSelect>
+              <IonButton className={styles.priceFilterButton} color="primary" expand="full" onClick={applyPriceFilter}>
+                Apply
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonGrid>
+          <IonRow className="ion-text-center">
+            <IonCol size="12">
+              <IonNote>
+                {searchResults && searchResults.length}{" "}
+                {searchResults.length !== 1 ? "products" : "product"} found
+              </IonNote>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+          {searchResults &&
+  searchResults
+    .slice(0, amountLoaded)
+    .map((product, index) => (
+      <ProductCard
+        key={`category_product_${index}`}
+        product={product}
+        index={index}
+        cartRef={cartRef}
+        category={category}
+      />
+    ))}
+          </IonRow>
+        </IonGrid>
+
+        <IonInfiniteScroll threshold="100px" onIonInfinite={fetchMore}>
+          <IonInfiniteScrollContent
+            loadingSpinner="bubbles"
+            loadingText="Fetching more..."
+          />
+        </IonInfiniteScroll>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default CategoryProducts;
